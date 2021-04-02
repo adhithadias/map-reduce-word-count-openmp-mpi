868c2094e9a9b7c45ea117b6eb9086e3c9c6b2ff
adhitha <adhithadias27@gmail.com>
2021-04-01 21:06:44 -0400

    add static file sending mechanism
    along with reading and mapping

---
+++ mpi_parallel.c
@@ -0,0 +1,196 @@
+#include <dirent.h>
+#include <errno.h>
+#include <mpi.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "util/hashTable.h"
+#include "util/queue.h"
+#include "util/util.h"
+
+#define FILE_NAME_BUF_SIZE 50
+#define TAG_COMM_REQ_DATA 0
+#define TAG_COMM_FILE_NAME 1
+
+int get_file_list(struct Queue *file_name_queue) {
+  DIR *dir;
+  struct dirent *in_file;
+
+  const char dirname[] = "./files/";
+  int file_count = 0;
+
+  // reference:
+  // https://stackoverflow.com/questions/11736060/how-to-read-all-files-in-a-folder-using-c
+  if ((dir = opendir(dirname)) == NULL) {
+    fprintf(stderr, "Error : Failed to open input directory - %s\n",
+            strerror(errno));
+    return 0;
+  }
+  while ((in_file = readdir(dir))) {
+    /* we don't want current and parent directories */
+    if (!strcmp(in_file->d_name, ".") || !strcmp(in_file->d_name, "..") ||
+        !strcmp(in_file->d_name, "./") || !strcmp(in_file->d_name, "../"))
+      continue;
+
+    /* Open directory entry file for common operation */
+    char *file_name = (char *)malloc(sizeof(char) * FILE_NAME_BUF_SIZE);
+    strcpy(file_name, dirname);
+    strcat(file_name, in_file->d_name);
+    enQueue(file_name_queue, file_name, strlen(file_name));
+    file_count++;
+  }
+  closedir(dir);
+  return file_count;
+}
+
+void populateQueue(struct Queue *q, char *file_name) {
+  // file open operation
+  FILE *filePtr;
+  if ((filePtr = fopen(file_name, "r")) == NULL) {
+    fprintf(stderr, "could not open file: [%p], err: %d, %s\n", filePtr, errno,
+            strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  // read line by line from the file and add to the queue
+  size_t len = 0;
+  char *line = NULL;
+  while (getline(&line, &len, filePtr) != -1) {
+    enQueue(q, line, len);
+  }
+  fclose(filePtr);
+  free(line);
+}
+
+void populateHashMap(struct Queue *q, struct hashtable *hashMap) {
+  struct node *node = NULL;
+
+  while (q->front) {
+    char str[q->front->len];
+    strcpy(str, q->front->line);
+    char *token;
+    char *rest = str;
+
+    // https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/
+    while ((token = strtok_r(rest, " ", &rest))) {
+      char *word = format_string(token);
+
+      if (strlen(word) > 0) {
+        node = add(hashMap, word, 0);
+        node->frequency++;
+      }
+      free(word);
+    }
+
+    deQueue(q);
+  }
+}
+
+int main(int argc, char **argv) {
+  MPI_Init(NULL, NULL);
+  int size, pid, p_name_len;
+  char p_name[MPI_MAX_PROCESSOR_NAME];
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+  MPI_Get_processor_name(p_name, &p_name_len);
+
+  /* file outputs for processes */
+  // declare a file
+  FILE *outfile;
+  // open a file whose name is based on the pid
+  char buf[10];
+  snprintf(buf, 10, "./pout/f%d", pid);
+  outfile = fopen(buf, "w");
+  // outfile = stdout;
+
+  MPI_Request request;
+  MPI_Status status;
+  int recv_pid;
+  int recv_len = 0;
+
+  int count = 0;
+  int done = 0;
+  int file_count = 0;
+  int done_sent_p_count = 0;
+
+  struct Queue *file_name_queue;
+  struct Queue *files_to_read;
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  char *file_names = (char *)malloc(sizeof(char) * FILE_NAME_BUF_SIZE * 1000);
+
+  if (pid == 0) {
+    file_name_queue = createQueue();
+    file_count = get_file_list(file_name_queue);
+
+    int num_files_to_send = file_count / size;
+
+    // iterate for all process ids in the comm world
+    for (int i = 0; i < size; i++) {
+      char *concat_files =
+          (char *)malloc(sizeof(char) * FILE_NAME_BUF_SIZE * num_files_to_send);
+      int len = 0;
+      // concat file names to one big char array for ease of sending
+      for (int j = 0; j < num_files_to_send; j++) {
+        if (j == 0) {
+          strcpy(concat_files, file_name_queue->front->line);
+        } else {
+          strcat(concat_files, file_name_queue->front->line);
+        }
+        len += file_name_queue->front->len + 1;
+        if (j != num_files_to_send - 1) {
+          strcat(concat_files, ",");
+        }
+        deQueue(file_name_queue);
+      }
+      if (i == 0) {  // send to other processes
+        strcpy(file_names, concat_files);
+        recv_len = len;
+      } else {
+        MPI_Send(concat_files, len + 1, MPI_CHAR, i, TAG_COMM_FILE_NAME,
+                 MPI_COMM_WORLD);
+      }
+    }
+  } else {
+    MPI_Status status;
+    MPI_Recv(file_names, FILE_NAME_BUF_SIZE * 1000, MPI_CHAR, 0,
+             TAG_COMM_FILE_NAME, MPI_COMM_WORLD, &status);
+    MPI_Get_count(&status, MPI_CHAR, &recv_len);
+  }
+
+  fprintf(outfile,
+          "received file name [%s] to pid %d from process 0, recv len %d\n",
+          file_names, pid, recv_len);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  // this indicates the end of reading section of the MPI
+
+  struct Queue *queue = createQueue();
+  struct hashtable *hash_table = createtable(CAPACITY);
+
+  // this can be run with multiple threads -- can be changed later
+  char *file;
+  while ((file = strtok_r(file_names, ",", &file_names))) {
+    if (strlen(file) > 0) {
+      fprintf(outfile, "file [%s]\n", file);
+
+      populateQueue(queue, file);          // read file
+      populateHashMap(queue, hash_table);  // map file
+    }
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  /*
+   * add reduction - hashtable should be communicated amoung the
+   * processes to come up with the final reduction
+   */
+  writeTable(hash_table, outfile, 0, hash_table->tablesize);
+
+  MPI_Finalize();
+  return 0;
+}
