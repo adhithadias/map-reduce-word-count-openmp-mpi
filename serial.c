939f05d70a1500a81d33282826fba3f2af0798b2
Mohammed Moiz <mmoiz@purdue.edu>
2021-04-10 18:10:41 -0400

    Added getopt support with -d -r and -g options

--- serial.c
+++ serial.c
@@ -3,107 +3,76 @@
 #include <string.h>
 #include <errno.h>
 #include <omp.h>
+#include <unistd.h>
 #include "util/queue.h"
 #include "util/hashTable.h"
 #include "util/util.h"
 
 extern int errno;
-
-void populateQueue(struct Queue *q, char *file_name)
-{
-    // file open operation
-    FILE *filePtr;
-    if ((filePtr = fopen(file_name, "r")) == NULL)
-    {
-        fprintf(stderr, "could not open file: [%p], err: %d, %s\n", filePtr, errno, strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    // read line by line from the file and add to the queue
-    size_t len = 0;
-    char *line = NULL;
-    int line_count = 0;
-    while (getline(&line, &len, filePtr) != -1)
-    {
-        enQueue(q, line, len);
-        line_count++;
-    }
-    // printf("line count %d, %s\n", line_count, file_name);
-    fclose(filePtr);
-    free(line);
-}
-
-void populateHashMap(struct Queue *q, struct hashtable *hashMap)
-{
-    struct node *node = NULL;
-    while (q->front)
-    {
-        char str[q->front->len];
-        strcpy(str, q->front->line);
-        char *token;
-        char *rest = str;
-
-        // https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/
-        while ((token = strtok_r(rest, " ", &rest)))
-        {
-            char *word = format_string(token);
-            if (strlen(word) > 0)
-            {
-                node = add(hashMap, word, 0);
-                node->frequency++;
-            }
-            free(word);
-        }
-        deQueue(q);
-    }
-}
-
-void reduce(struct hashtable **hash_tables, struct hashtable *final_table, int file_count, int location)
-{
-    struct node *node = NULL;
-    int i;
-    for (i = 0; i < file_count; i++)
-    {
-        if (hash_tables[i] == NULL || hash_tables[i]->table[location] == NULL)
-        {
-            continue;
-        }
-        // if (final_table->table[location] == NULL) {
-        //     final_table->table[location] = hash_tables[i]->table[location];
-        // }
-
-        struct node *current = hash_tables[i]->table[location];
-        if (current == NULL)
-            continue;
-
-        while (current != NULL)
-        {
-            node = add(final_table, current->key, 0);
-            node->frequency += current->frequency;
-            current = current->next;
-        }
-    }
-}
+int DEBUG_MODE = 0;
+int PRINT_MODE = 1;
 
 int main(int argc, char **argv)
 {
-    char files_dir[] = "./files";  // TODO: This should be taken from argv
-    int file_count;
-    int DEBUG_MODE = 1;
-
+    int opt;
+    char files_dir[FILE_NAME_BUF_SIZE] = "./files/"; // TODO: This should be taken from argv
+    int file_count = 0;
+    int repeat_files = 1;
     double global_time = -omp_get_wtime();
     double local_time;
 
+    /********************** Parsing User inputs from run command with getopt **********************************/
+    // https://stackoverflow.com/questions/17877368/getopt-passing-string-parameter-for-argument
+    while ((opt = getopt(argc, argv, "d:r:g")) != -1)
+    {
+        switch (opt)
+        {
+        case 'd':
+            printf("Files Directory given: \"%s\"\n", optarg);
+            strcpy(files_dir, optarg);
+            break;
+        case 'r':
+            printf("Files to be repeated: %s time(s)\n", optarg);
+            repeat_files = (int)atol(optarg);
+            break;
+        case 'g':
+            printf("Running in debug mode\n");
+            DEBUG_MODE = 1;
+            break;
+        case ':':
+            fprintf(stderr, "Option -%c requires an argument to be given\n", optopt);
+            return 1;
+        default:
+            break;
+        }
+    }
+    /**********************************************************************************************************/
+
+    /********************** Creating and populating FilesQueue ************************************************/
     struct Queue *file_name_queue;
     file_name_queue = createQueue();
 
-    if (DEBUG_MODE) printf("Files Directory: %s\n", files_dir);
-    file_count = get_file_list(file_name_queue, files_dir);
+    if (PRINT_MODE)
+    {
+        printf("\nReading files in Directory: %s\n", files_dir);
+        printf("Reading files %d time(s)\n", repeat_files);
+    }
+    for (int i = 0; i < repeat_files; i++)
+    {
+        int files = get_file_list(file_name_queue, files_dir);
+        if (files == -1)
+        {
+            printf("Check input directory and rerun! Exiting!\n");
+            return 1;
+        }
+        file_count += files;
+    }
     printf("Files in Queue: %d\n", file_count);
     /**********************************************************************************************************/
 
     /********************** Populating Queues and HashTables by reading files in the FilesQueue ***************/
-    if (DEBUG_MODE) printf("\nPopulating Queues and HashTables by reading files in the FilesQueue\n");
+    if (PRINT_MODE)
+        printf("\nPopulating Queues and HashTables by reading files in the FilesQueue\n");
     local_time = -omp_get_wtime();
     struct Queue **queues;
     struct hashtable **hash_tables;
@@ -119,11 +88,13 @@ int main(int argc, char **argv)
         populateHashMap(queues[i], hash_tables[i]);
     }
     local_time += omp_get_wtime();
-    if (DEBUG_MODE) printf("Done Populating! Time taken: %f\n", local_time);
+    if (PRINT_MODE)
+        printf("Done Populating! Time taken: %f\n", local_time);
     /**********************************************************************************************************/
 
     /********************** Reducing the populated words in HashTable *****************************************/
-    if (DEBUG_MODE) printf("\nReducing the populated words in HashTable\n");
+    if (PRINT_MODE)
+        printf("\nReducing the populated words in HashTable\n");
     local_time = -omp_get_wtime();
     struct hashtable *final_table = createtable(50000);
     for (int i = 0; i < 50000; i++)
@@ -134,20 +105,24 @@ int main(int argc, char **argv)
         }
     }
     local_time += omp_get_wtime();
-    if (DEBUG_MODE) printf("Done Reducing! Time taken: %f\n", local_time);
+    if (PRINT_MODE)
+        printf("Done Reducing! Time taken: %f\n", local_time);
     /**********************************************************************************************************/
 
     /********************** Writing Reduced counts to output file *********************************************/
-    if (DEBUG_MODE) printf("\nWriting Reduced counts to output file\n");
+    if (PRINT_MODE)
+        printf("\nWriting Reduced counts to output file\n");
     local_time = -omp_get_wtime();
     // printTable(final_table);
     writeFullTable(final_table, "./output/serial/0.txt");
     local_time += omp_get_wtime();
-    if (DEBUG_MODE) printf("Done Writing! Time taken: %f\n", local_time);
+    if (PRINT_MODE)
+        printf("Done Writing! Time taken: %f\n", local_time);
     /**********************************************************************************************************/
 
     /********************** Clearing the heap allocations for Queues and HashTables ***************************/
-    if (DEBUG_MODE) printf("\nClearing the heap allocations for Queues and HashTables\n");
+    if (PRINT_MODE)
+        printf("\nClearing the heap allocations for Queues and HashTables\n");
     local_time = -omp_get_wtime();
     for (int i = 0; i < file_count; i++)
     {
@@ -158,7 +133,8 @@ int main(int argc, char **argv)
     free(queues);
     free(hash_tables);
     local_time += omp_get_wtime();
-    if (DEBUG_MODE) printf("Done Clearing! Time taken: %f\n", local_time);
+    if (PRINT_MODE)
+        printf("Done Clearing! Time taken: %f\n", local_time);
     /**********************************************************************************************************/
 
     global_time += omp_get_wtime();
--- util/util.h
+++ util/util.h
@@ -7,16 +7,19 @@
 #include "queue.h"
 
 extern int errno;
+extern int DEBUG_MODE;
 
 #define FILE_NAME_BUF_SIZE 50
 
-int get_file_list(struct Queue *file_name_queue, char* dirpath)
+int get_file_list(struct Queue *file_name_queue, char *dirpath)
 {
     DIR *dir;
     struct dirent *in_file;
 
-    char dirname[256] = "./files/";
-    // strcpy(dirname, dirpath);  // TODO: Currently dirpath variable is dummy. Use the input from this variable
+    char dirname[FILE_NAME_BUF_SIZE];
+    // Assuming Linux only. Null character needs to be added to avoid garbage
+    char directory_seperator[2] = "/\0";
+    strcpy(dirname, dirpath);
     int file_count = 0;
 
     // reference:
@@ -24,7 +27,7 @@ int get_file_list(struct Queue *file_name_queue, char* dirpath)
     if ((dir = opendir(dirname)) == NULL)
     {
         fprintf(stderr, "Error : Failed to open input directory - %s\n", strerror(errno));
-        return 0;
+        return -1;
     }
     while ((in_file = readdir(dir)))
     {
@@ -34,12 +37,18 @@ int get_file_list(struct Queue *file_name_queue, char* dirpath)
             continue;
 
         /* Open directory entry file for common operation */
-        char *file_name = (char *)malloc(sizeof(char) * FILE_NAME_BUF_SIZE);
+        // mallocing 3 times the directory buffer size for file_name
+        char *file_name = (char *)malloc(sizeof(char) * FILE_NAME_BUF_SIZE * 3);
         strcpy(file_name, dirname);
+        strcat(file_name, directory_seperator);
         strcat(file_name, in_file->d_name);
+        if (DEBUG_MODE)
+            printf("Queing file: %s\n", file_name);
         enQueue(file_name_queue, file_name, strlen(file_name));
         file_count++;
     }
+    if (DEBUG_MODE)
+        printf("Done Queing all files\n\n");
     closedir(dir);
     return file_count;
 }
@@ -52,8 +61,7 @@ char *format_string(char *original)
     int len = strlen(original);
     char *word = (char *)malloc(len * sizeof(char));
     int c = 0;
-    int i;
-    for (i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
         if (isalnum(original[i]) || original[i] == '\'')
         {
@@ -63,4 +71,79 @@ char *format_string(char *original)
     }
     word[c] = '\0';
     return word;
+}
+
+void populateQueue(struct Queue *q, char *file_name)
+{
+    // file open operation
+    FILE *filePtr;
+    if ((filePtr = fopen(file_name, "r")) == NULL)
+    {
+        fprintf(stderr, "could not open file: [%p], err: %d, %s\n", filePtr, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // read line by line from the file and add to the queue
+    size_t len = 0;
+    char *line = NULL;
+    int line_count = 0;
+    while (getline(&line, &len, filePtr) != -1)
+    {
+        enQueue(q, line, len);
+        line_count++;
+    }
+    // printf("line count %d, %s\n", line_count, file_name);
+    fclose(filePtr);
+    free(line);
+}
+
+void populateHashMap(struct Queue *q, struct hashtable *hashMap)
+{
+    struct node *node = NULL;
+    while (q->front)
+    {
+        char str[q->front->len];
+        strcpy(str, q->front->line);
+        char *token;
+        char *rest = str;
+
+        // https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/
+        while ((token = strtok_r(rest, " ", &rest)))
+        {
+            char *word = format_string(token);
+            if (strlen(word) > 0)
+            {
+                node = add(hashMap, word, 0);
+                node->frequency++;
+            }
+            free(word);
+        }
+        deQueue(q);
+    }
+}
+
+void reduce(struct hashtable **hash_tables, struct hashtable *final_table, int file_count, int location)
+{
+    struct node *node = NULL;
+    for (int i = 0; i < file_count; i++)
+    {
+        if (hash_tables[i] == NULL || hash_tables[i]->table[location] == NULL)
+        {
+            continue;
+        }
+        // if (final_table->table[location] == NULL) {
+        //     final_table->table[location] = hash_tables[i]->table[location];
+        // }
+
+        struct node *current = hash_tables[i]->table[location];
+        if (current == NULL)
+            continue;
+
+        while (current != NULL)
+        {
+            node = add(final_table, current->key, 0);
+            node->frequency += current->frequency;
+            current = current->next;
+        }
+    }
 }
